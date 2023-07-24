5c70d12d8aaffbfb013569a6775b9f12efdb78b5
Royroy205 <leroyjr2344@live.com>
2023-07-24 13:42:47 -0500

    operators done

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int additionResults = a + b;
+            //int substractionResult = a - b;
+            //int multiplicationResult = a * b;
+            //int divisionResult = a / b;
+            //int Modulus = a % b;
+             
+            int a = 17;
+            int b = 4;
+            int Quotient = a / b;
+            int Remainder = (a % b);
+
+            Console.WriteLine($"{a}/{b} is {Quotient}, Remainder {Remainder}");
+
+            PrintAreaofCircle();
+
+
+
         }
-    }
+         
+        public static double AreaOfCircle(double radius)
+        {
+           
+           
+            return Math.PI * radius * radius;
+
+
+        }
+        public static void PrintAreaofCircle()
+        {
+            Console.WriteLine("What is the radius of the circle?");
+
+            double radius = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
+
+
+
+
+        }
+
+       
+            
+
+
+
+
+
+
+
+       
+    
+    
+    } 
 }
